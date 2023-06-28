492d7f94ff591a7f00a98f2730967a0480e21ce6
Jonatan Paiva <perrolha@gmail.com>
2023-06-28 18:28:01 -0300

    visual updates

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,28 @@
 export const dataFake = [
     {
         "id":"1",
-        "title":"TÍTULO DO CARD GRANDE AQUI",
-        "description":"Descrição do card grande nesse parágrafo",
-        "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+        "photoCover":"https://chennai.vit.ac.in/wp-content/uploads/2021/08/Types-of-Software-Developer.jpg",
+        "title":"Sobre Mim",
+        "description":"Estudante de Análise e Desenvolvimento de Sistemas, formado em Publicidade e Propaganda. Mais de 10 anos de experiência em Design Gráfico e Ilustração. Em transição de carreira para a área de TI."
+
     },
     {
         "id":"2",
-        "title":"Título do card pequeno",
-        "description":"ola",
-        "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+        "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
+        "title":"Desenvolvedor",
+        "description":"Conhecimento de HTML, CSS, Bootstrap, JavaScripts, Typescript, PHP, MySQL, Angular, Java, Git;",
+    },
+    {
+        "id":"3",
+        "photoCover":"https://www.totaljobs.com/advice/wp-content/uploads/graphic-designer-job-description.jpg",
+        "title":"Designer Gráfico",
+        "description":"Experiência em Photoshop, Illustrator, Premiere, After Effects, Indesign",
+    },
+    {
+        "id":"4",
+        "photoCover":"https://t.ctcdn.com.br/5vi5r9TkMFYTYQ22zdhgEeXVmV4=/1400x788/smart/i6689.jpeg",
+        "title":"Podcaster",
+        "description":"Participante e editor do podcast Neuronautas, disponível no Spotify e demais agregadores",
     }
 ]
   
