ae5cfcd978487b541de01bd3a4c7efd20c420e10
Jonatan Paiva <perrolha@gmail.com>
2023-06-28 18:04:11 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"TÍTULO DO CARD GRANDE AQUI",
+        "description":"Descrição do card grande nesse parágrafo",
+        "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+    },
+    {
+        "id":"2",
+        "title":"Título do card pequeno",
+        "description":"ola",
+        "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+    }
+]
+  
